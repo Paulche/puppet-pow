3fab729a17a9de01e30ac7586f5a431ec87d5f81
Paul Chechetin <paulche@yandex.ru>
2014-02-15 22:42:40 +0400

    Implement post install steps

---
+++ manifests/config.pp
@@ -0,0 +1,14 @@
+
+class pow::config {
+  $home       = "/Users/${::boxen_user}"
+  $logdir     = "${boxen::config::logdir}/pow"
+
+  $service_firewall_name  = 'dev.pow.firewall' 
+  $service_powd_name      = 'dev.pow.powd'
+
+  $firewall_plist   = "/Library/LaunchDaemons/${service_firewall_name}.plist"
+  $powd_plist       = "${home}/Library/LaunchAgents/${service_powd_name}.plist"
+  
+  $pow_path   = "${boxen::config::homebrewdir}/bin/pow"
+  $node_path  = "${boxen::config::homebrewdir}/bin/node"
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,58 @@
 #
 #     include pow
 class pow {
+  
+  include pow::config
+
+  $home = "/Users/${::boxen_user}"
+
   package { 'pow':
     ensure   => 'latest',
     provider => 'homebrew'
+  }
+
+  file { ["${pow::config::home}/Library/Application Support/Pow", "${pow::config::home}/Library/Application Support/Pow/Hosts"]:
+    ensure  => directory,
+  } 
+
+  file { "${pow::config::home}/.pow":
+    target  => "${home}/Library/Application Support/Pow/Hosts",
+    ensure  => link,
+  }
+
+  file { $pow::config::logdir:
+    ensure => present,
+  }
+
+  file { 'firewall_plist':
+    path    => $pow::config::firewall_plist,
+    content => template('pow/firewall.plist'),
+    owner   => "root",
+    group   => "wheel",
+  }
+
+  file { 'powd_plist': 
+    path    => $pow::config::powd_plist,
+    content => template('pow/powd.plist'),
+  }
+
+  exec { 'append port to dev resolver':
+    command => "echo '\nport 20559' >> /etc/resolver/dev",
+    user    => "root",
+    unless  => "grep -c 20559 /etc/resolver/dev",
+  }
+
+  exec { 'load firewall rule':
+    command => "launchctl load ${pow::config::firewall_plist}",
+    user    => 'root',
+    unless  => "ipfw list | grep 'fwd 127.0.0.1,20559 tcp from any to me dst-port 80 in'",
+    require => File['firewall_plist'],
+  }
+
+  exec { 'load powd service':
+    command => "launchctl load ${pow::config::powd_plist}",
+    user    => $::boxen_user,
+    unless  => "launchctl list | grep ${pow::config::service_powd_name}",
+    require => File['powd_plist'],
   }
 }
